6f923597b3b8d03ab1b9fabeea280c0444cffd9e
pedrovsiqueira <pedro.v.siqueira@gmail.com>
2020-08-18 15:33:14 -0300

    feat: create input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,78 @@
+import React, {
+  InputHTMLAttributes,
+  useRef,
+  useEffect,
+  useState,
+  useCallback,
+  ChangeEvent,
+} from 'react';
+import { useField } from '@unform/core';
+import { IconBaseProps } from 'react-icons';
+import { FiAlertCircle } from 'react-icons/fi';
+import {
+  Container, StyledInput, Error, StyledLabel,
+} from './styles';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon?: React.ComponentType<IconBaseProps>;
+  label: string;
+  handleChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input2: React.FC<InputProps> = ({
+  name,
+  icon: Icon,
+  label,
+  handleChange,
+  ...rest
+}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { fieldName, error, registerField } = useField(name);
+
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+  }, []);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <>
+      <StyledLabel htmlFor={name}>{label}</StyledLabel>
+      <Container
+        isErrored={!!error}
+        isFocused={isFocused}
+        data-testid="input-container"
+      >
+        <StyledInput
+          isErrored={!!error}
+          onFocus={handleInputFocus}
+          onBlur={handleInputBlur}
+          onChange={handleChange}
+          ref={inputRef}
+          {...rest}
+        />
+
+        {error && (
+          <Error title={error}>
+            <FiAlertCircle color="#f8a186" size={20} />
+          </Error>
+        )}
+      </Container>
+    </>
+  );
+};
+
+export default Input2;
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,72 @@
+import styled, { css } from 'styled-components';
+import Tooltip from '../../components/Tooltip';
+
+interface StyledProps {
+  isFocused?: boolean;
+  isErrored: boolean;
+}
+
+export const Container = styled.div<StyledProps>`
+  width: 100%;
+  max-width: 615px;
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  box-sizing: border-box;
+  background-color: #ecf0f3;
+  box-shadow: 18px 18px 30px #d1d9e6,
+    inset 10px 10px 30px rgba(255, 255, 255, 0.4);
+  border-radius: 17px;
+  margin-bottom: 24px;
+
+  ${(props) => props.isErrored
+    && css`
+      background: #ecf0f3;
+      border: 2px solid #f8a186;
+      box-shadow: 18px 18px 30px #bfcee6,
+        inset 10px 10px 30px rgba(255, 196, 196, 0.4);
+      color: #f8a186;
+    `}
+
+  ${(props) => props.isFocused
+    && css`
+      box-shadow: 18px 18px 30px #bfcee6,
+        inset 10px 10px 30px rgba(255, 255, 255, 0.4);
+      border: 2px solid #a6bba8;
+
+      &::placeholder {
+        color: #a2a2a2;
+      }
+    `}
+
+    & :disabled {
+    background: #d1d9e6;
+    color: #5c5c5c;
+    border-radius: 17px;
+  }
+`;
+
+export const StyledInput = styled.input<StyledProps>`
+  background: transparent;
+  border: 0;
+  padding: 15px;
+  width: 100%;
+
+  &::placeholder {
+    color: #a2a2a2;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  padding: 12px 10px 0 0;
+`;
+
+export const StyledLabel = styled.label`
+  font-size: 2rem;
+  font-weight: 600;
+  letter-spacing: 0.02em;
+  font-variant: small-caps;
+  margin-bottom: 8px;
+  display: block;
+`;
