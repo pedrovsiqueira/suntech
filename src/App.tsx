0a6a596454e51da67b0db44693aca48d053a2daf
pedrovsiqueira <pedro.v.siqueira@gmail.com>
2020-08-19 00:24:25 -0300

    feat: add router, provider and globalstyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import GlobalStyle from './styles/global';
+import Routes from './routes';
+import AppProvider from './hooks/index';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Router>
+      <AppProvider>
+        <Routes />
+      </AppProvider>
+      <GlobalStyle />
+    </Router>
   );
 }
 
