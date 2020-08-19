88dc84a962925d5fc3f7607d61683cba2a190ba4
pedrovsiqueira <pedro.v.siqueira@gmail.com>
2020-08-19 00:30:37 -0300

    feat: create ToastContainer component

---
+++ src/components/ToastContainer/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container } from './styles';
+import { ToastMessage } from '../../hooks/toast';
+import Toast from './Toast';
+
+interface ToastContainerProps {
+  messages: ToastMessage[];
+}
+
+const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => (
+  <Container>
+    {messages.map((message) => (
+      <Toast key={message.id} message={message} />
+    ))}
+  </Container>
+);
+
+export default ToastContainer;
